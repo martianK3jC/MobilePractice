1bb915ab6c648747746fb100f7ef1b4f9c12c226
Roger Ceniza <rogerceniza2024@outlook.com>
2025-03-31 04:23:33 +0800

    Added Simple List View with click and long click listeners

---
+++ app/src/main/java/com/android/mobilepractice/SimpleListViewActivity.kt
@@ -0,0 +1,31 @@
+package com.android.mobilepractice
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import com.android.mobilepractice.utils.toast
+
+class SimpleListViewActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simple_list_view)
+
+        val userList = listOf("Kesha", "Jane", "Ceniza", "Katsuki", "Bakugo", "Karma", "Akabane", "Kei", "Tsukishima")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, userList)
+
+        val listView = findViewById<ListView>(R.id.list_view_user_list)
+        listView.adapter = arrayAdapter
+
+        listView.setOnItemLongClickListener { _, _, position, _ ->
+            toast("Long press item ${position + 1} with data ${userList[position]}")
+            true // Ensure the event is consumed
+        }
+
+        listView.setOnItemClickListener { _, _, position, _ ->
+            toast("Item ${position + 1} with data ${userList[position]}")
+        }
+    }
+}
